76e94b200cb8fa40fba05bc3b83b446241c1c4bf
Mstiekema <merijnstiekema@outlook.com>
2020-02-04 10:07:19 +0100

    Changed the intervals back to the multiplication

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
 	"logo": "https://svsticky.nl/wp-content/uploads/logo-sticky-licht.png",
 	"interval": {
-		"load": 300000,
-		"next": 8000
+		"load": 15*60*1000,
+		"next": 8*1000
 	},
 	"endpoints": {
 		"root": "https://koala.svsticky.nl/api",
