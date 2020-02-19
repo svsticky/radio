a304b798bea170b8d562e098838944b14f0d5fae
Mstiekema <merijnstiekema@outlook.com>
2020-02-19 19:14:01 +0100

    Using new logo, fixed #38

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"logo": "https://svsticky.nl/wp-content/uploads/logo-sticky-licht.png",
+	"logo": "https://images.ctfassets.net/7cqe14fu3dhm/3b328YmKeNAX9jUOLTH35C/8f8213bf0afb6ea9d39a94720caa8544/LogolWit.svg",
 	"interval": {
 		"load": 15*60*1000,
 		"next": 8*1000
