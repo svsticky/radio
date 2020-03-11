ca42451db92aff170138dcf7b8f779f8bf8ffcde
Rick van Hoef <rickvhoef@gmail.com>
2020-03-11 14:28:51 +0100

    use file-loader plugin to add index and css to build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App';
+import css from './style.css';
+import html from './index.html';
 
-render(<App apiRoot='https://koala.svsticky.nl/api'/>, document.getElementById('root'));
+render(<App apiRoot='https://koala.svsticky.nl/api' />, document.getElementById('root'));
