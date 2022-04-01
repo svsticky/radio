998a2b6a0671c8727533647094535a487b0249ef
Mstiekema <merijnstiekema@outlook.com>
2022-04-01 14:23:30 +0200

    Going through quotes randomly now

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import GetContent from './Contentful';
 
-let content = [];
-
 GetContent('quotes', entries => {
-  content = entries !== null ? entries.map(entry => entry.fields) : [];
+  const quotes = entries !== null ? entries.map(entry => entry.fields) : [];
+  window.localStorage.setItem('AllQuotes', JSON.stringify(quotes));
 });
 
 // Get a random quote
-function getRandomQuote() {
+function getRandomQuote(quotes) {
   let quote = {
-    text: "",
-    person: "" 
-  }
-  let i = Math.floor(Math.random() * content.length);
-  if (content.length > 0) {
+    text: '',
+    person: ''
+  };
+
+  let i = Math.floor(Math.random() * quotes.length);
+  if (quotes.length > 0) {
     quote = {
-      text: content[i].text,
-      person: content[i].person
-    }
+      text: quotes[i].text,
+      person: quotes[i].person
+    };
+
+    // Delete quote after using it.
+    quotes = quotes.filter(quote => quote != quotes[i]);
+    window.localStorage.setItem('quotes', JSON.stringify(quotes));
   }
 
   return quote;
 }
 
 export default function Quotes() {
-  let quote = getRandomQuote();
+  let quotes = JSON.parse(localStorage.getItem('quotes') || '[]');
+
+  if (quotes == null || quotes == undefined || quotes.length == 0 || quotes == []) {
+    quotes = JSON.parse(window.localStorage.getItem('AllQuotes'));
+    window.localStorage.setItem('quotes', JSON.stringify(quotes));
+  }
+
+  let quote = getRandomQuote(quotes);
+
   return (
     <article className="quoteSection">
       <section className="quoteBackground">
