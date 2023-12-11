1cd76e85b7d309b9a0e8f26d637a14f7490b75a3
Mstiekema <github@mstiekema.nl>
2023-12-11 18:42:02 +0100

    Added snow to radio if it's dec, jan or feb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App';
+import snowjs from './snow/snow.js';
+import snow from './snow/snow.css';
 import css from './style.css';
 import html from './index.html';
 
