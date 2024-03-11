cee6da1f6d0490696c0b004d80a799fcc6374fbc
Luke Bleijenbergh <lukeheeftmail@gmail.com>
2024-03-11 22:40:07 +0100

    added rainbows during pride month

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -64,9 +64,22 @@ export default class Activity extends Component {
     if (props.participant_counter != null)
       participants = ` (${props.participant_counter})`;
 
+    let day = new Date();
+    let pride = false;
+    if (day.getMonth() == 5) {
+      pride = true;
+    }
+
     let className = "activity";
     if (props.active) {
       className += " active";
+      if (pride) {
+        className += " activeRainbow";
+      }
+    }
+
+    if (pride) {
+      className += " rainbow";
     }
 
     return (
