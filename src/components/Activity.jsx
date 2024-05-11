18d3adbda698f52742ac3115a396dd9237a4da55
SilasPeters <69711739+SilasPeters@users.noreply.github.com>
2024-05-11 20:26:04 +0200

    fix: replaced deprecated code

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,28 +1,21 @@
-import { Component } from 'react';
-import { findDOMNode } from 'react-dom';
+import { useEffect, useRef } from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import scrollIntoView from 'scroll-into-view';
 
-export default class Activity extends Component {
+const Activity = ({ active, name, start_date, end_date, has_start_time, has_end_time, participant_counter }) => {
+  const activityRef = useRef(null);
 
-  componentDidMount() {
-    this.ensureVisible();
-  }
-
-  componentDidUpdate() {
-    this.ensureVisible();
-  }
-
-  ensureVisible() {
-    if (this.props.active) {
-      scrollIntoView(findDOMNode(this), {
+  // Ensure that the current activity is visible
+  useEffect(() => {
+    if (active && activityRef.current) {
+      scrollIntoView(activityRef.current, {
         time: 500,
       });
     }
-  }
+  }, [active]);
 
-  sameDay(d, t = new Date()) {
+  const sameDay = (d, t = new Date()) => {
     return (
       d.getDate() === t.getDate() && // getDate returns day number in month...
       d.getMonth() === t.getMonth() &&
@@ -30,63 +23,62 @@ export default class Activity extends Component {
     );
   }
 
-  makeStartDate() {
-    var sd = this.props.start_date;
-    if (this.sameDay(sd) && this.props.has_start_time)
+  const makeStartDate = () => {
+    var sd = start_date;
+    if (sameDay(sd) && has_start_time)
       return moment(sd).format('HH:mm');
 
-    if (this.props.has_start_time) return moment(sd).format('dddd DD-MM HH:mm');
+    if (has_start_time) return moment(sd).format('dddd DD-MM HH:mm');
 
     return moment(sd).format('dddd DD-MM');
   }
 
-  makeEndDate() {
-    var ed = this.props.end_date;
+  const makeEndDate = () => {
+    var ed = end_date;
     if (!ed) return null;
-    if (this.sameDay(ed, this.props.start_date)) {
-      if (this.props.has_end_time) return moment(ed).format('HH:mm');
+    if (sameDay(ed, start_date)) {
+      if (has_end_time) return moment(ed).format('HH:mm');
       return null; // Same as start_date
     }
 
-    if (this.props.has_end_time) return moment(ed).format('dddd DD-MM HH:mm');
+    if (has_end_time) return moment(ed).format('dddd DD-MM HH:mm');
 
     return moment(ed).format('dddd DD-MM');
   }
 
-  render() {
-    const props = this.props;
-    const startDate = this.makeStartDate();
-    const endDate = this.makeEndDate();
-    var participants = '';
+  const startDate = makeStartDate();
+  const endDate = makeEndDate();
+  var participants = '';
 
-    if (props.participant_counter != null)
-      participants = ` (${props.participant_counter})`;
+  if (participant_counter != null)
+    participants = ` (${participant_counter})`;
 
-    let className = 'activity';
-    if (props.active) {
-      className += ' active';
-    }
-
-    return (
-      <li className={className}>
-        <h1>
-          {props.name}
-          {participants}
-        </h1>
-        <time>{startDate}</time>
-        {endDate ? <time> - {endDate}</time> : null}
-      </li>
-    );
+  let className = 'activity';
+  if (active) {
+    className += ' active';
   }
+
+  return (
+    <li className={className} ref={activityRef}>
+      <h1>
+        {name}
+        {participants}
+      </h1>
+      <time>{startDate}</time>
+      {endDate ? <time> - {endDate}</time> : null}
+    </li>
+  );
 }
 
-// Explain expected types, for early error detection
+// Define the the types of this component, for early error detection
 Activity.propTypes = {
   active: PropTypes.bool.isRequired,
-  start_date: PropTypes.string.isRequired,
-  end_date: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  start_date: PropTypes.instanceOf(Date).isRequired,
+  end_date: PropTypes.instanceOf(Date),
   has_start_time: PropTypes.bool.isRequired,
   has_end_time: PropTypes.bool.isRequired,
-  name: PropTypes.string.isRequired,
   participant_counter: PropTypes.number,
 };
+
+export default Activity;
