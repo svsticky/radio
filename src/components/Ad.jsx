ec60ff032eb60ba3c49f4867c6b9ad83a6984150
Job Vonk <sticcpistt@icloud.com>
2024-07-06 17:10:05 +0200

    refactor: turned Ad into a function component

--- src/components/Ad.jsx
+++ src/components/Ad.jsx
@@ -1,53 +1,49 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Poster from './Poster';
 import GetContent from '../helpers/contentful';
 
-export default class Ad extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ads: [],
-    };
+export default function Ad({ current, onChange }) {
+  const ads = useAds();
 
-    GetContent('ads', (entries) => {
-      this.setState({
-        ads: entries !== null ? entries.map((entry) => entry.fields) : [],
-      });
-    });
+  if (ads.length > 0 && current >= ads.length - 1)
+    onChange(true);
+
+  const currentAd = ads[current];
+  if (ads.length <= 0) {
+    return (
+      <div>
+        <ul className="advertisement"></ul>
+        <Poster poster={'https://public.svsticky.nl/.hidden/Backup-Ad.png'} />
+      </div>
+    );
+  } else if (currentAd.fullscreen) {
+    return (
+      <div className="full-advertisement">
+        <Poster poster={`https:${currentAd.poster.fields.file.url}`}></Poster>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <ul className="advertisement">
+          <h1>{currentAd.title}</h1>
+          <p>{currentAd.description}</p>
+        </ul>
+        <Poster poster={`https:${currentAd.poster.fields.file.url}`}></Poster>
+      </div>
+    );
   }
+}
 
-  render() {
-    if (this.state.ads.length <= 0)
-      return (
-        <div>
-          <ul className="advertisement"></ul>
-          <Poster poster={'https://public.svsticky.nl/.hidden/Backup-Ad.png'} />
-        </div>
-      );
+function useAds() {
+  const [ads, setAds] = useState([]);
 
-    if (this.props.current >= this.state.ads.length - 1)
-      this.props.onChange(true);
+  useEffect(() => {
+    GetContent('ads', entries => setAds((entries ?? []).map(entry => entry.fields)));
+  }, []);
 
-    let currentAd = this.state.ads[this.props.current];
-    if (currentAd.fullscreen) {
-      return (
-        <div className="full-advertisement">
-          <Poster poster={`https:${currentAd.poster.fields.file.url}`}></Poster>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <ul className="advertisement">
-            <h1>{currentAd.title}</h1>
-            <p>{currentAd.description}</p>
-          </ul>
-          <Poster poster={`https:${currentAd.poster.fields.file.url}`}></Poster>
-        </div>
-      );
-    }
-  }
+  return ads;
 }
 
 // Explain expected types, for early error detection
