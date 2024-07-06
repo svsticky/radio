110176499cd3ccff09ceea29391e4d99fcd45f1f
Job Vonk <sticcpistt@icloud.com>
2024-07-06 16:39:22 +0200

    refactor: turned Activities into a function component

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,79 +1,33 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Activity from './Activity';
 import Poster from './Poster';
 import { KOALA_ACTIVITY_ENDPOINT } from '../helpers/env';
 import PropTypes from 'prop-types';
 
-export default class Activities extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activities: [],
-    };
+export default function Activities({ current, onChange }) {
+  const activities = useActivities();
 
-    this.loadData();
-  }
+  if (activities.length > 0) {
+    if (current >= activities.length - 1)
+      onChange(true);
 
-  //Utility function to change dates from activities to actual Date objects
-  setDate(activity) {
-    return Object.assign(
-      {},
-      activity,
-      { has_start_time: activity.start_date.indexOf('T') > -1 },
-      {
-        has_end_time: activity.end_date && activity.end_date.indexOf('T') > -1,
-      },
-      { start_date: new Date(activity.start_date) },
-      activity.end_date ? { end_date: new Date(activity.end_date) } : null
+    const currentActivity = activities[current];
+    return (
+      <div>
+        <ul className="activities">
+          {activities.map((activity, i) =>
+            <Activity
+              key={i}
+              {...activity}
+              active={activity === currentActivity}
+              />
+          )}
+        </ul>
+        <Poster poster={currentActivity ? currentActivity.poster : null} />
+      </div>
     );
-  }
-
-  // Get activities
-  loadData() {
-    fetch(KOALA_ACTIVITY_ENDPOINT)
-      // Fix activity dates and sort them on start_date
-      .then((resp) => resp.json())
-      .then((activities) =>
-        activities.map(this.setDate).sort((a, b) => a.start_date - b.start_date)
-      )
-      .then((activities) => {
-        this.setState({ activities: activities.filter((act) => act.poster) });
-      });
-  }
-
-  componentDidMount() {
-    // Set up interval.
-    // Every this.props.loadInterval, new events are loaded from koala.
-    this.dataLoader = setInterval(
-      this.loadData.bind(this),
-      parseInt(import.meta.env.VITE_LOAD_INTERVAL)
-    );
-  }
-
-  render() {
-    if (this.state.activities.length > 0) {
-      if (this.props.current >= this.state.activities.length - 1)
-        this.props.onChange(true);
-
-      let currentActivity = this.state.activities[this.props.current];
-      return (
-        <div>
-          <ul className="activities">
-            {this.state.activities.map((activity, i) => {
-              return (
-                <Activity
-                  key={i}
-                  {...activity}
-                  active={activity === currentActivity}
-                />
-              );
-            })}
-          </ul>
-          <Poster poster={currentActivity ? currentActivity.poster : null} />
-        </div>
-      );
-    }
-    return <div />;
+  } else {
+    return <></>;
   }
 }
 
@@ -82,3 +36,33 @@ Activities.propTypes = {
   current: PropTypes.number.isRequired,
   onChange: PropTypes.func.isRequired,
 };
+
+function setDate(activity) {
+  return Object.assign({
+    ...activity,
+    has_start_time: activity.start_date.indexOf('T') > -1,
+    has_end_time: activity.end_date && activity.end_date.indexOf('T') > -1,
+    start_date: new Date(activity.start_date)
+  }, activity.end_date ? { end_date: new Date(activity.end_date) } : null);
+}
+
+function useActivities() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch(KOALA_ACTIVITY_ENDPOINT)
+        // Fix activity dates and sort them on start_date
+        .then(resp => resp.json())
+        .then(activities =>
+          setActivities(activities
+            .filter(act => act.poster)
+            .map(setDate)
+            .sort((a, b) => a.start_date - b.start_date)));
+    }, Number(import.meta.env.VITE_LOAD_INTERVAL));
+
+    return () => clearInterval(interval);
+  });
+
+  return activities;
+}
