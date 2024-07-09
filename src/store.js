ffa21a245fe416b0fcf717f2bf61d63284a236ac
Job Vonk <sticcpistt@icloud.com>
2024-07-09 22:29:43 +0200

    refactor: create api slice for quotes

--- src/store.js
+++ src/store.js
@@ -1,16 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import activities from "./store/activities";
 import ads from "./store/ads";
+import boardMessages from "./store/boardMessages";
+import quotes from "./store/quotes";
 
 const store = configureStore({
   reducer: {
     [activities.reducerPath]: activities.reducer,
-    [ads.reducerPath]: ads.reducer
+    [ads.reducerPath]: ads.reducer,
+    [boardMessages.reducerPath]: boardMessages.reducer,
+    [quotes.reducerPath]: quotes.reducer
   },
   middleware(getDefaultMiddleware) {
     return getDefaultMiddleware()
       .concat(activities.middleware)
-      .concat(ads.middleware);
+      .concat(ads.middleware)
+      .concat(boardMessages.middleware)
+      .concat(quotes.middleware);
   }
 });
 
