08a8b14703f0f405ea176425b603476481d929f0
Job Vonk <sticcpistt@icloud.com>
2024-09-05 15:19:25 +0200

    docs: added docs to quotes slice

--- src/store/quotes.js
+++ src/store/quotes.js
@@ -7,13 +7,20 @@ const quotes = createSlice({
     quoteIndex: 0
   },
   reducers: {
+    /**
+     * Select a random quote from the list of available quotes and remove
+     * it, so that it cannot be reused again until all quotes are displayed.
+     */
     nextQuote(state) {
       const hi = state.availableQuotes.length - 1;
       const availableQuoteIndex = Math.floor(Math.random() * hi);
 
       [state.quoteIndex] = state.availableQuotes.splice(availableQuoteIndex, 1);
     },
-    resetQuotes(state, action) {
+    /**
+     * Reset the list of availabe quotes to a new list of indices from 0 to the number of quotes.
+     */
+    resetAvailableQuotes(state, action) {
       state.availableQuotes = new Array(action.payload)
         .fill(0)
         .map((_, i) => i);
@@ -23,9 +30,13 @@ const quotes = createSlice({
 
 export const { nextQuote } = quotes.actions;
 
+/**
+ * A synchronous thunk that resets the quotes state into an unused valid state.
+ * @param payload the total number of quotes
+ */
 export function resetQuotes(payload) {
   return (dispatch, _getState) => {
-    dispatch(quotes.actions.resetQuotes(payload));
+    dispatch(quotes.actions.resetAvailableQuotes(payload));
     dispatch(quotes.actions.nextQuote());
   }
 }
