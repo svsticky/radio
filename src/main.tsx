500792ff5a29765662f575f2ece465f1f13eb4d2
Job Vonk <sticcpistt@icloud.com>
2024-11-24 17:39:44 +0100

    refactor: condensed snow.css using scss
    css wont be the major language in the repository anymore, finally!

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import store from './store/index.js';
 
 import './index.css';
-import './snow/snow.css';
+import './snow.scss';
 
 const root = document.getElementById('root');
 if (root) {
@@ -17,6 +17,23 @@ if (root) {
       </React.StrictMode>
     </Provider>,
   );
+
+  const snowflakeContainer = document.createElement('div');
+  document.body.appendChild(snowflakeContainer);
+
+  // Check if the date is between 1st of December and March 1st
+  const today = new Date();
+  const mm = today.getMonth() + 1;
+  const isWinter = mm >= 12 || mm < 3;
+
+  if (isWinter) {
+    // Create 200 snowflakes
+    for (let i = 0; i < 200; i++) {
+      const snowflake = document.createElement('div');
+      snowflake.className = 'snowflake';
+      snowflakeContainer.appendChild(snowflake);
+    }
+  }
 } else {
   throw new Error('Cannot find root element to bind to');
 }
