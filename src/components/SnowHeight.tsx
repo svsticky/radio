7f3ce892633786a89dec422a2d6ed41d3eec0b6d
Job Vonk <sticcpistt@icloud.com>
2024-11-25 14:31:24 +0100

    feat: background for snow height page (#166)

--- src/components/SnowHeight.tsx
+++ src/components/SnowHeight.tsx
@@ -3,10 +3,13 @@ import { useTimer } from '../StateMachine';
 export default function SnowHeight() {
   useTimer();
   return (
-    <img
-      src={import.meta.env.VITE_SNOW_HEIGHT_URL}
-      id="snowHeight"
-      alt="Snow height meter"
-    />
+    <>
+      <img src="/snow_mountain.jpeg" id="snowHeightBg" />
+      <img
+        src={import.meta.env.VITE_SNOW_HEIGHT_URL}
+        id="snowHeight"
+        alt="Snow height meter"
+      />
+    </>
   );
 }
