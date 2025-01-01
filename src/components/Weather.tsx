7493724b2b9b1546dd5bec0604d4b9a65db755b1
KoenDR06 <koen.de.ruiter@hotmail.com>
2025-01-01 16:40:54 +0100

    Applied requested changes from StickyPiston

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,95 +1,41 @@
-import React, { useEffect, useState } from 'react';
-
-const VITE_WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
-
-function fetchWeather(
-  signal: AbortSignal,
-  setWeather: React.Dispatch<unknown>,
-) {
-  fetch(
-    `https://weerlive.nl/api/weerlive_api_v2.php?key=${VITE_WEATHER_API_KEY}&locatie=52.08718206955104,5.165697854286648`,
-    { signal },
-  )
-    .then((response) =>
-      response.json().then((data) => {
-        setWeather(data);
-      }),
-    )
-    .catch(() => {
-      setWeather(JSON.parse('{"liveweer": [{ "temp": 0, "image": "block"}]}'));
-    });
-}
+import { useWeatherQuery } from '../store/api';
 
 export default function Weather() {
-  const [weather, setWeather] = useState(
-    JSON.parse('{"liveweer": [{ "temp": 0, "image": "block"}]}'),
-  );
+  const { data: weather, isSuccess } = useWeatherQuery(undefined, {
+    pollingInterval: 600000, // 10 * 60 * 1000, 10 minutes
+  });
 
-  useEffect(() => {
-    const controller = new AbortController();
+  if (!isSuccess) return <></>;
 
-    fetchWeather(controller.signal, setWeather);
+  const iconMap = new Map([
+    ['mist', 'mist'],
+    ['buien', 'rainy'],
+    ['regen', 'rainy'],
+    ['sneeuw', 'rainy'],
+    ['bewolkt', 'rainy'],
+    ['onbewolkt', 'sunny'],
+    ['nachtmist', 'bedtime'],
+    ['hagel', 'weather_hail'],
+    ['wolkennacht', 'bedtime'],
+    ['bliksem', 'thunderstorm'],
+    ['helderenacht', 'bedtime'],
+    ['nachtbewolkt', 'bedtime'],
+    ['halfbewolkt_regen', 'rainy'],
+    ['zwaarbewolkt', 'filter_drama'],
+    ['halfbewolkt', 'partly_cloudy_day'],
+    ['lichtbewolkt', 'partly_cloudy_day'],
+  ]);
 
-    const timerId = setInterval(
-      () => {
-        fetchWeather(controller.signal, setWeather);
-        if (weather == undefined) {
-          return <div className="weather-wrapper"></div>;
-        }
-      },
-      // Make the call every 10 minutes
-      10 * 60 * 1000,
-    );
-
-    return () => {
-      clearInterval(timerId);
-      controller.abort();
-    };
-  }, []);
-
-  function getImageSource(img: string) {
-    switch (img) {
-      case 'mist':
-        return 'mist';
-      case 'buien':
-        return 'rainy';
-      case 'regen':
-        return 'rainy';
-      case 'hagel':
-        return 'weather_hail';
-      case 'sneeuw':
-        return 'rainy';
-      case 'bewolkt':
-        return 'rainy';
-      case 'bliksem':
-        return 'thunderstorm';
-      case 'onbewolkt':
-        return 'sunny';
-      case 'nachtmist':
-        return 'bedtime';
-      case 'wolkennacht':
-        return 'bedtime';
-      case 'halfbewolkt':
-        return 'partly_cloudy_day';
-      case 'helderenacht':
-        return 'bedtime';
-      case 'nachtbewolkt':
-        return 'bedtime';
-      case 'zwaarbewolkt':
-        return 'filter_drama';
-      case 'lichtbewolkt':
-        return 'partly_cloudy_day';
-      case 'halfbewolkt_regen':
-        return 'rainy';
-      default:
-        return 'block';
-    }
+  function getIconName(image: string) {
+    const res = iconMap.get(image);
+    if (res == undefined) return 'block';
+    else return res;
   }
 
   return (
     <div className="weather-wrapper">
       <span className="material-symbols-outlined weather-icon">
-        {getImageSource(weather.liveweer[0].image)}
+        {getIconName(weather.liveweer[0].image)}
       </span>
       <span className="weather-text">{weather.liveweer[0].temp + ' °C'}</span>
     </div>
