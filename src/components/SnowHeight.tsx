04f1388e9b408ae0f80176a5dc880cca707d3cd7
Job Vonk <sticcpistt@icloud.com>
2025-11-19 15:51:46 +0100

    feat: updated SnowHeight to use an iframe instead of an image (#217)
    * Updated SnowHeight to use an iframe instead of an image
    
    * Added border-radius to snow height page

--- src/components/SnowHeight.tsx
+++ src/components/SnowHeight.tsx
@@ -4,11 +4,11 @@ export default function SnowHeight() {
   useTimer();
   return (
     <>
-      <img src="/snow_mountain.jpeg" id="snowHeightBg" />
-      <img
+      <img src="/snow_mountain.jpeg" className="snowHeightBg" />
+      <iframe
         src={import.meta.env.VITE_SNOW_HEIGHT_URL}
         id="snowHeight"
-        alt="Snow height meter"
+        className="snowHeight"
       />
     </>
   );
