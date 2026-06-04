dfa3bdfb58d96a1006ef3f2d616aea6e76ac2565
Julius <julius.kurvers@gmail.com>
2026-06-04 12:00:36 +0200

    updated vite-env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -19,6 +19,14 @@ interface ImportMetaEnv {
   readonly VITE_SNOW_HEIGHT_URL?: string;
 
   readonly VITE_WEATHER_API_TOKEN: string;
+
+  readonly VITE_SHOW_ACTIVITIES_PAGE?: string;
+  readonly VITE_SHOW_ADVERTISEMENT_PAGE?: string;
+  readonly VITE_SHOW_BOARDTEXT_PAGE?: string;
+  readonly VITE_SHOW_COMMITS_PAGE?: string;
+  readonly VITE_SHOW_COMMITTEECLASH_PAGE?: string;
+  readonly VITE_SHOW_QUOTES_PAGE?: string;
+  readonly VITE_SHOW_SNOWHEIGHT_PAGE?: string;
 }
 
 interface ImportMeta {
